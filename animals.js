9980718b0988af064d98b7c5e32e7f9a95522967
chrismw <chrismw7579@gmail.com>
2020-01-18 13:01:16 -0800

    added script file

---
+++ animals.js
@@ -0,0 +1,46 @@
+let animals = [
+    {
+        name: 'black widow',
+        type: 'spider',
+        locations: ['United States', 'southern Europe', 'Asia', 'Australia', 'Africa', 'South America', 'Galapagos'],
+        toxin: 'venom',
+        toxicity: 'high',
+        symptoms: [' severe pain, burning, swelling, and redness at the site', ' Nausea or vomiting', 'Difficulty breathing', 'Severe abdominal pain or cramping', 'Excessive sweating', ' Rash and itching', 'Swollen eyelids', 'Weakness or tremors', 'can be fatal in young children'],
+        pictures: [],
+        aggressiveness: 'low',
+        genus: 'Latrodectus',
+        remedies: ['wash the area with soap and water', 'apply a cold washcloth or ice pack wrapped in cloth to the area', 'elevate it to prevent swelling', 'apply an antibiotic cream or lotion', 'children should be take to the doctor'],
+        
+
+    },
+    {
+        name: 'Brown Recluse',
+        type: 'spider',
+        locations: ['south United States', 'central United States'],
+        toxin: 'venom',
+        toxicity: 'very high',
+        symptoms: ['Pain or redness at the site', 'A deep sore (ulcer)', 'Fever', 'Chills', 'Nausea', 'Joint pain', 'Feeling weak', 'Seizures or coma (very rare)'],
+        pictures: [],
+        aggressiveness: 'low',
+        genus: 'Loxosceles',
+        remedies: ['wash the area with soap and water', 'apply a cold washcloth or ice pack wrapped in cloth to the area', 'elevate it to prevent swelling', 'apply an antibiotic cream or lotion', 'children should be take to the doctor'],
+        
+    },
+    {
+        name: '',
+        type: '',
+        locations: [],
+        toxin: '',
+        toxicity: '',
+        symptoms: [],
+        pictures: [],
+        aggressiveness: '',
+        genus: '',
+        remedies: [],
+        
+    },
+
+
+
+]
+
