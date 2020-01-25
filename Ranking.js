48c6b906a2793ed06d6eb2fedba85a202f377081
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-25 12:04:57 -0800

    added stat counter

--- Ranking.js
+++ Ranking.js
@@ -1,16 +1,17 @@
 // ranks threats based on severity
 let crime_list = document.querySelector("#crime-list");
 let crimes = ['shooting', 'assault', 'robbery', 'arson', 'burglary', 'theft', 'arrest','vandalism', 'other'];
+let crimeCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function rankOffences(data) {
     let count = 0;
-    console.log(data.length);
     for (var i = 0; i < crimes.length; i++) {
       
         
         for(var j = 0; j < data.length; j++) {
             if(data[j].type.toLowerCase() === crimes[i]) {
                 count++;
-                
+                crimeCount[i]++;
                 
                 let newRow = document.createElement("tr");
                 let number = document.createElement("td");
@@ -20,6 +21,7 @@ function rankOffences(data) {
                 let link = document.createElement("td");
                 let ref = document.createElement("a");
                 ref.setAttribute('href', data[j].link);
+                ref.setAttribute("target", "_blank");
                 ref.textContent = "link"
                 number.textContent = count;
                 type.textContent = data[j].type;
@@ -34,6 +36,20 @@ function rankOffences(data) {
                 crime_list.appendChild(newRow);
             }
         }
-        console.log("count " + count);
+
     }
+    crimeStats();
+}
+
+function crimeStats() {
+  let list = document.getElementById("crimeStats");
+  let elements = list.children;
+
+  for(let i = 0; i < crimeCount.length; i++) {
+    let stat = crimeCount[i];
+    elements[i].children[0].textContent = stat;
+    if(stat > 1) {
+      elements[i].children[1].textContent = elements[i].children[1].textContent + "s";
+    }
+  }
 }
--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 // ==========================================================================================================================================
 // ==========================================================================================================================================
 // This code fetches the JSON from the API
+
 const key = 'This-api-key-is-for-commercial-use-exclusively.Only-entities-with-a-Spotcrime-contract-May-use-this-key.Call-877.410.1607.';
 let lat = 40;
 //for the US, the longitude will always be negative
 let lon = -122;
-let radius = 10; // this is in miles
+let radius = 100; // this is in miles
+
+
 let url = `https://api.spotcrime.com/crimes.json?&lat=${lat}&lon=${lon}&radius=${radius}&key=${key}`;
+
 async function fetchJson() {
   $.ajax({
     type: 'GET',
@@ -32,30 +36,44 @@ async function fetchJson() {
     }
   });
 }
+
+//fetches the json, and updates the list
 fetchJson();
 // ==========================================================================================================================================
 // ==========================================================================================================================================
 // ==========================================================================================================================================
 // This code should loap the map onto the page
-$(document).foundation();
-// Initialize and add the map
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+
+var map = new google.maps.Map(document.getElementById("map-canvas"), {
+  center: {
+      lat: 27.72,
+      lng: 85.36
+  },
+  zoom: 15
+});
+var marker = new google.maps.Marker({
+  position: {
+      lat: 27.72,
+      lng: 85.36
+  },
+  map:map,
+  draggable: true
+});
+var searchBox = new google.maps.places.SearchBox(document.getElementById("mapsearch"));
+google.maps.event.addListener(searchBox, "places_changed", function() {
+  var places = searchBox.getPlaces();
+  var bounds = new google.maps.LatLngBounds();
+  var i, place;
   
-}
-initMap()
+  for(i = 0; place = places[i]; i++) {
+      console.log(place.geometry.location);
+      bounds.extend(place.geometry.location);
+      marker.setPosition(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+  map.setZoom(50);
+});
+
 // ==========================================================================================================================================
 // ==========================================================================================================================================
 // ==========================================================================================================================================
-
-
-
-
-
