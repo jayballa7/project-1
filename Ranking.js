e3bcf75ad720c81c97ff5530506d92f689b8c132
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-26 12:20:38 -0800

    added responsive styling and general styling

--- Ranking.js
+++ Ranking.js
@@ -2,40 +2,49 @@
 // ==========================================================================================================================================
 // ==========================================================================================================================================
 // ranks threats based on severity
-let crime_list = document.querySelector("#crime-list");
+let crime_list = document.querySelector("#crime_list");
 let input = document.querySelector("#input");
 let inputBtn = document.querySelector(".btn");
 let prev = document.querySelector("#prev");
 let pageBtn = document.querySelector("#page");
 let next = document.querySelector("#next");
 
+// ranking variables
 let crimes = ['shooting', 'assault', 'robbery', 'arson', 'burglary', 'theft', 'arrest', 'vandalism', 'other'];
 let crimeCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let listLimit = input.value;
 let page = 0;
 let dataCashe = [];
 
+// results per page input box
 inputBtn.addEventListener('click', function() {
   listLimit = input.value;
   crime_list.innerHTML = '';
   rankOffences(dataCashe);
 });
 
+// previous page button
 prev.addEventListener('click', function() {
     if(page > 0) {
       page--;
     }
     crime_list.innerHTML = '';
     rankOffences(dataCashe);
-})
+});
 
+// next page button
 next.addEventListener('click', function() {
     page++;
     crime_list.innerHTML = '';
     rankOffences(dataCashe);
-})
-
+});
+// ==========================================================================================================================================
+// ==========================================================================================================================================
+// ==========================================================================================================================================
+// this function clears the results on the page if there are any, and then adds new ones based on the location
 function rankOffences(data) {
+  crimeCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    crime_list.innerHTML = "";
     let count = 0;
     dataCashe = data;
     for (var i = 0; i < crimes.length; i++) {
@@ -89,9 +98,6 @@ function crimeStats() {
   for(let i = 0; i < crimeCount.length; i++) {
     let stat = crimeCount[i];
     elements[i].children[0].textContent = stat;
-    if(stat > 1) {
-      elements[i].children[1].textContent = elements[i].children[1].textContent + "s";
-    }
   }
 }
 // ==========================================================================================================================================
